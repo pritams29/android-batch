387965b8df551da3c7ef51fdcf38d2ef3b43fb4d
Lenovo <pritammgaikwad@gmail.com>
2019-06-25 12:08:31 +0530

    androiddaily

---
+++ androiddaily/relativelayout/src/main/java/com/example/relativelayout/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.relativelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.basic_wtsap)
+    }
+}
